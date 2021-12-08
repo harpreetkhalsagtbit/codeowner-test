56b5040d01e43b6e9a4f3ddecb77935e20b83084
harpreetAdPushup <harpreet.singh@adpushup.com>
2021-12-08 14:47:29 +0530

    udpate

--- protectedCode.js
+++ protectedCode.js
@@ -1,3 +1,4 @@
 console.log('Protected file created by harpreetAdpushup - update 2');
 console.log('updated by harpreetkhalsagtbi')
 console.log('update from non-owne')
+console.log('direct push non-owner');
